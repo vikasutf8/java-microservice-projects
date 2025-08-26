890b83e7916ead2411981b637dca151b6958baab
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-08-26 23:39:55 +0530

    Feat: API of byte/chuck dividing video base on range

--- readme.md
+++ readme.md
@@ -29,5 +29,55 @@ Content-Type: application/json
 }
 ```
 
+## List of All Videos
 
+### Request
 
+```
+GET /api/v1/video/list
+```
+
+### Response
+
+```
+HTTP/1.1 200 OK
+Content-Type: application/json
+[
+    {
+        videoId: "videoId",
+        title: "Video Title",
+        description: "Video Description",
+        contentType: "video/mp4",
+        filePath: "video/videoId.mp4"
+    },
+    {
+        videoId: "videoId",
+        title: "Video Title",
+        description: "Video Description",
+        contentType: "video/mp4",
+        filePath: "video/videoId.mp4"
+    }
+]
+```
+
+## Streaming Video
+
+### Request
+
+```
+GET /api/v1/video/stream/{videoId}
+```
+
+### Response
+
+```
+HTTP/1.1 200 OK
+Content-Type: video/mp4
+
+```
+### ISSUEs  --not byte  Meant complete file 10GB transmission
+### making byte wise as broadcasting ...
+
+-  Bandwidth issue 
+- network conjection issue
+- wasted Resources 
--- src/main/java/Streaming/Application/Video/controller/VideoControllers.java
+++ src/main/java/Streaming/Application/Video/controller/VideoControllers.java
@@ -1,17 +1,24 @@
 package Streaming.Application.Video.controller;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,7 +48,7 @@ public class VideoControllers {
         video.setTitle(title);
         video.setDescription(description);
         video.setContentType(file.getContentType());
-        video.setVideoId(UUID.randomUUID().toString()); 
+        video.setVideoId(UUID.randomUUID().toString());
 
         Video videoSaved = videoService.save(video, file);
         if (videoSaved != null) {
@@ -52,7 +59,6 @@ public class VideoControllers {
         }
     }
 
-
     // getAll video List
     @GetMapping("/list")
     public ResponseEntity<List<Video>> getAllVideos() {
@@ -60,25 +66,88 @@ public class VideoControllers {
     }
 
     // streaming video
-// http://localhost:8080/api/v1/video/stream/{videoId}
+    // http://localhost:8080/api/v1/video/stream/{videoId}
     @GetMapping("/stream/{videoId}")
     public ResponseEntity<Resource> streamVideo(@PathVariable String videoId) {
 
         Video video = videoService.get(videoId);
-       String contentType = video.getContentType();
-       String filePath = video.getFilePath();
-       Resource resource = new FileSystemResource(filePath);
+        String contentType = video.getContentType();
+        String filePath = video.getFilePath();
+        Resource resource = new FileSystemResource(filePath);
 
-       if(contentType ==null){
-        contentType = "application/octet-stream";
-       }
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
 
-
-
-       return ResponseEntity
-       .ok()
-       .contentType(MediaType.parseMediaType(contentType))
-       .body(resource);
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .body(resource);
     }
 
+    // streaming video by title
+
+    @GetMapping("/stream/range/{videoId}")
+    public ResponseEntity<Resource> streamVideoByTitle(@PathVariable String videoId,
+            @RequestHeader(value = "Range", required = false) String rangeHeader) {
+
+        Video video = videoService.get(videoId);
+        Path filePath = Paths.get(video.getFilePath());
+        String contentType = video.getContentType();
+
+        Resource resource = new FileSystemResource(filePath);
+
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+        long fileLength = filePath.toFile().length();
+        if (rangeHeader == null) {
+            return ResponseEntity
+                    .ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .body(resource);
+        }
+
+        // calculate range
+        long rangeStart;
+        long rangeEnd;
+
+        // bytes 1002-1343. or bytes=1002-1343
+        rangeHeader.replace("bytes=", "");
+        String[] ranges = rangeHeader.split("-");
+        rangeStart = Long.parseLong(ranges[0]);
+        if (ranges.length > 1) {
+            rangeEnd = Long.parseLong(ranges[1]);
+        } else {
+            rangeEnd = fileLength - 1;
+        }
+
+        if (rangeEnd > fileLength - 1) {
+            rangeEnd = fileLength - 1;
+        }
+        InputStream inputStream;
+        try {
+            inputStream = new FileInputStream(filePath.toFile());
+            inputStream.skip(rangeStart);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .build();
+        }
+
+        long contentLength = rangeEnd - rangeStart + 1;
+        org.springframework.http.HttpHeaders httpHeaders = new org.springframework.http.HttpHeaders();
+        httpHeaders.add("Content-Range", "bytes " + rangeStart + "-" + rangeEnd + "/" + fileLength);
+        httpHeaders.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        httpHeaders.add("Pragma", "no-cache");
+        httpHeaders.add("Expires", "0");
+        httpHeaders.add("X-Content-Type-Options", "nosniff");
+        httpHeaders.setContentLength(contentLength);
+
+        return ResponseEntity
+                .status(HttpStatus.PARTIAL_CONTENT)
+                .headers(httpHeaders) 
+                .contentType(MediaType.parseMediaType(contentType))
+                .body(new InputStreamResource(inputStream));
+    }
 }
