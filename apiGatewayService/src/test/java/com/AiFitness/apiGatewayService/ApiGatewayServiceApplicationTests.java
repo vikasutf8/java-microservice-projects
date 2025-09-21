64d098acaeb5ec08875a0900732bb5abaf71fa2e
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-09-21 13:43:37 +0530

    apiGateway Service implimented

---
+++ apiGatewayService/src/test/java/com/AiFitness/apiGatewayService/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AiFitness.apiGatewayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
